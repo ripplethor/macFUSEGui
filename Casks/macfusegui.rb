4500b0e53ba0529784d8d132e7892be093700e53
RippleThor <5694805+ripplethor@users.noreply.github.com>
2026-02-25 23:29:50 +0000

    Release v0.1.12

--- Casks/macfusegui.rb
+++ Casks/macfusegui.rb
@@ -1,8 +1,8 @@
 cask "macfusegui" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "0.1.11"
-  sha256 arm: "f9024af965676de6152bcfe663c30b87341f1862a8139d942e15b9b08cef9f58", intel: "1c9e71c01f63ef24549bd2b332fb4c137dbdb3e782d6ebbc19ac9624d829b9c1"
+  version "0.1.12"
+  sha256 arm: "16a1826f60661c027cecf3a59052d266a146bc95f1552b4f998ac4402db7f250", intel: "f8b8cdf140bf692c6601735a18e5ff82e2fd23512e9ffa1f68b37f26b27bd139"
 
   url "https://github.com/ripplethor/macfuseGUI/releases/download/v#{version}/macfuseGui-v#{version}-macos-#{arch}.dmg",
       verified: "github.com/ripplethor/macfuseGUI/"
